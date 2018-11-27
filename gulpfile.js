9939e4e869792a97f20358cb743141eb62dd9cfe
Sebastian Zuloaga <sebastian_zuloagad@hotmail.com>
2018-11-27 14:20:06 -0500

    Initialise GIT, GULP, SASS, and working directory

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/*Dependency List Attribution*/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/style.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+  });
